2ed709f0eb242ce001688c54cae4051cf7db3ac6
StopGuard <stopguard@bk.ru>
2021-02-10 11:53:02 +0500

    Question 1 is ready

--- Question1.py
+++ Question1.py
@@ -5,9 +5,24 @@
     3. до суток: <h> час <m> мин <s> сек;
     4. * в остальных случаях: <d> дн <h> час <m> мин <s> сек.
 """
-duration = ''
-while not duration.isdigit():
-    duration = input('Введите количество секунд: ')
-else:
-    duration = int(duration)
+_duration = ''                                  # объявляем переменную времени для нормального запуска цикла
+max_duration_list = [60, 60, 24]                # описываем макс количества сек мин ч
+duration_list = []                              # объявляем список для получения в него количеств сек мин ч д
+duration_str = [' сек.', ' мин ', ' час ', ' дн '] # подписи для единиц измерения
+output_duration = ''                            # переменная для вывода полученного отрезка времени
 
+while not _duration.isdigit():   # проверяем чтобы переменная была числовой
+    _duration = input('Введите время в секундах: ')  # просим ввод переменной, пока она не станет числовой
+else:
+    duration = int(_duration)    # после получения числовой переменной преобразовываем ее в число
+
+for i in range(3):  # пробегаем по списку единиц времени
+    duration_list.append(duration % max_duration_list[i])   # забираем остаток от деления на макс количество единиц
+    duration = duration // max_duration_list[i]             # переводим время в следующую единицу измерения
+
+duration_list.append(duration)  # добавляем к списку дни
+
+for i, t in enumerate(duration_list):   # проходим по спискам подписей и полученного списка собирая данные в кучу
+    output_duration = str(t) + duration_str[i] + output_duration if t > 0 else output_duration # если время 0 пропускаем
+
+print(output_duration)
